d015ab3da381bb43ed905ba930c0341d8091e481
ZaferBerkay <zaferberkay1@gmail.com>
2022-10-30 17:29:19 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+  const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  return (
+    <>
+      <AddTutorial getTutorials={getTutorials} />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
+    </>
+  );
+};
+
+export default Home;
